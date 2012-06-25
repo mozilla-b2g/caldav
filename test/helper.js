96683161e43fc0101c74f0875d1a2d445afe7e8d
James Lal <james@lightsofapollo.com>
2012-06-25 16:44:39 +0200

    test/helper add xhr tests

--- test/helper.js
+++ test/helper.js
@@ -1,30 +1,52 @@
-var chai = require('chai'),
-    fs = require('fs');
+(function() {
+  var chai = require('chai'),
+      fs = require('fs'),
+      path = require('path');
 
-chai.Assertion.includeStack = true;
-assert = chai.assert;
+  chai.Assertion.includeStack = true;
+  assert = chai.assert;
 
-loadSample = function(file, cb) {
-  var root = __dirname + '/../samples/';
-  fs.readFile(root + file, 'utf8', function(err, contents) {
-    cb(err, contents);
-  });
-};
-
-defineSample = function(file, cb) {
-  suiteSetup(function(done) {
-    loadSample(file, function(err, data) {
-      if (err) {
-        done(err);
-      }
-      cb(data);
-      done();
+  loadSample = function(file, cb) {
+    var root = __dirname + '/../samples/';
+    fs.readFile(root + file, 'utf8', function(err, contents) {
+      cb(err, contents);
     });
-  });
-};
+  };
 
-requireLib = function(lib) {
-  return require(__dirname + '/../lib/webcals/' + lib);
-};
+  defineSample = function(file, cb) {
+    suiteSetup(function(done) {
+      loadSample(file, function(err, data) {
+        if (err) {
+          done(err);
+        }
+        cb(data);
+        done();
+      });
+    });
+  };
 
-Webcals = require('../lib/webcals/webcals.js');
+  requireLib = function(lib) {
+    return require(__dirname + '/../lib/webcals/' + lib);
+  };
+
+  requireSupport = function(lib) {
+    return require(__dirname + '/support/' + lib);
+  }
+
+  Webcals = require('../lib/webcals/webcals.js');
+
+  if (typeof(window) === 'undefined') {
+    //in node we need to hack Webcals to do the right thing.
+    var oldRequire = Webcals.require;
+
+    Webcals.require = function exportRequireDev(path) {
+      if (path.indexOf('support') === 0) {
+        path = __dirname + '/' + path;
+        return require(path);
+      }
+      return oldRequire(path);
+    }
+  }
+
+}());
+
---
+++ test/webcals/request/abstract_test.js
@@ -0,0 +1,13 @@
+requireLib('xhr');
+requireLib('request/abstract');
+
+suite('webcals/request/abstract.js', function() {
+  var subject;
+  var Abstract;
+
+  suiteSetup(function() {
+    Abstract = Webcals.require('request/abstract');
+  });
+
+
+});
